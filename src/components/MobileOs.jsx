744450c58ad990aadcd090c98e5f3498612f0cd8
rameshmane7218 <rameshmane7218@gmail.com>
2022-05-19 17:56:40 +0530

    added react mobile os assignment

---
+++ src/components/MobileOs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const MobileOs = () => {
+  return (
+    <div className="mobileOs">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+    </div>
+  );
+};
